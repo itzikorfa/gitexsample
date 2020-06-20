e14bdc9f1e778b8248f6b287cfd332ef81028802
Yizhak Orfanian <itzikorfa@users.noreply.github.com>
2020-06-20 12:58:27 +0300

    new file

--- git_exsample.py
+++ git_exsample.py
@@ -1,4 +1,14 @@
 import git
-new_repo = git.Repo.init('new_repo')
-# this is an update
-git.Repo.clone_from("https://github.com/itzikorfa/gitexsample", "gitExample")
+# repo = git.Repo.init('gitExample2')
+try:
+    git.Repo.clone_from("https://github.com/itzikorfa/gitexsample", "gitExample3")
+except Exception as e:
+    repo = git.Repo.init('gitExample3')
+    print(e)
+    repo.remote().pull()
+#
+#
+# if repo.is_dirty():
+#     print("changes exists")
+# repo.remote().pull()
+# # git.Repo.clone_from("https://github.com/itzikorfa/gitexsample", "gitExample")
